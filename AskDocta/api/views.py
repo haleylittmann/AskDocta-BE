08b501d1047b17594e9da5afe1618597c92ec443
mcleodaj <mcleoda@yahoo.com>
2019-11-20 17:37:41 -0500

    Don't show profiles without names for moderation

--- AskDocta/api/views.py
+++ AskDocta/api/views.py
@@ -35,7 +35,7 @@ def profiles(request):
         return redirect('/')
     if not request.user.profile.phone:
         return redirect('/profile/edit')
-    ps = Profile.objects.all().filter(first_name__isnull=False)
+    ps = Profile.objects.all().filter(first_name!="")
     profiles = []
     for p in ps:
         email = p.user.email
